4e0c6e674b6d86fe90066aa660828f8072e015ba
Hinome <57831472+RealHinome@users.noreply.github.com>
2022-12-10 17:19:44 +0100

    Add documentation for init 📝

--- src/database/postgres.rs
+++ src/database/postgres.rs
@@ -1,5 +1,13 @@
 use tokio_postgres::{Client, NoTls, Error};
 
+/// Create tables for PostgreSQL and connection between code and
+/// database.
+///
+/// Example
+/// ```rust
+/// let mut client = init().await;
+/// client.query("SELECT * FROM warn WHERE affected_id = ?", &[&"bad_user"]);
+/// ```
 pub async fn init() -> Result<Client, Error> {
     let (client, connection) = tokio_postgres::connect(&format!("host=localhost user={} password={} dbname={}",
                                                                 dotenv::var("DB_USERNAME").expect("Missing env `DB_USERNAME`"),

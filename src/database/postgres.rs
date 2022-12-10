a9dac241c104bb318dbdf827763ab9a1ff96db20
Hinome <57831472+RealHinome@users.noreply.github.com>
2022-12-10 17:12:40 +0100

    Clearer 🎨

--- src/database/postgres.rs
+++ src/database/postgres.rs
@@ -13,24 +13,24 @@ pub async fn init() -> Result<Client, Error> {
         }
     });
 
-    client.query("CREATE TABLE IF NOT EXISTS warn (
-        id              BIGSERIAL PRIMARY KEY,
-        affected_id     TEXT NOT NULL,
-        user_id         TEXT NOT NULL,
-        ip              TEXT,
-        country         TEXT,
-        reason          SMALLINT NOT NULL
+    client.query("CREATE TABLE IF NOT EXISTS warn ( \
+        id              BIGSERIAL PRIMARY KEY, \
+        affected_id     TEXT NOT NULL, \
+        user_id         TEXT NOT NULL, \
+        ip              TEXT, \
+        country         TEXT, \
+        reason          SMALLINT NOT NULL \
     )", &[]).await?;
 
     // Date eg. 1999-01-08
     // punishment: 0 suspend account | 1 deleted post | 2 airblog suspended acc.
-    client.query("CREATE TABLE IF NOT EXISTS punishment (
-        id              BIGSERIAL PRIMARY KEY,
-        user_id         TEXT NOT NULL,
-        mod_id          TEXT NOT NULL,
-        reason          SMALLINT NOT NULL,
-        punishment      INT NOT NULL,
-        expiration      date
+    client.query("CREATE TABLE IF NOT EXISTS punishment ( \
+        id              BIGSERIAL PRIMARY KEY, \
+        user_id         TEXT NOT NULL, \
+        mod_id          TEXT NOT NULL, \
+        reason          SMALLINT NOT NULL, \
+        punishment      INT NOT NULL, \
+        expiration      date \
     )", &[]).await?;
 
     Ok(client)

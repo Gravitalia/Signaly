98a1cb00c21446802db946493b4806bdbc51b443
Hinome <57831472+RealHinome@users.noreply.github.com>
2023-05-20 16:12:04 +0200

    Add timer to delete account

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,6 +1,9 @@
 use jsonwebtoken::{decode, Algorithm, Validation, DecodingKey, TokenData};
+use chrono::{Duration as ChronoDuration, Utc};
 use serde::{Serialize, Deserialize};
+use std::time::Duration;
 use anyhow::Result;
+use crate::model;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Claims {
@@ -87,4 +90,84 @@ pub async fn alert(vanity: String, platform: String, affected_user: String, reas
         .await?;
 
     Ok(())
+}
+
+/// Read the config content and return it
+pub fn read() -> model::Config {
+    let config: model::Config = serde_yaml::from_reader(std::fs::File::open("config.yaml").expect("Could not find config.yaml file")).expect("Could not read values of config.yaml file");
+    config
+}
+
+/// Suspend a user with his vanity
+pub async fn suspend(vanity: String, platform: Option<String>) -> Result<()> {
+    if platform.is_some() {
+        reqwest::Client::new().post(format!("{}account/suspend?vanity={}", platform.unwrap(), vanity))
+        .header("Authorization", dotenv::var("GLOBAL_AUTH")?)
+        .body("")
+        .send()
+        .await?;
+    } else {
+        // Send request to Autha
+        reqwest::Client::new().post(format!("{}/account/suspend?vanity={}", dotenv::var("AUTHA_URL")?, vanity))
+        .header("Authorization", dotenv::var("GLOBAL_AUTH")?)
+        .body("")
+        .send()
+        .await?;
+
+    // Send request to all other services
+    for service in read().services {
+        reqwest::Client::new().post(format!("{}/account/suspend?vanity={}", service, vanity))
+        .header("Authorization", dotenv::var("GLOBAL_AUTH")?)
+        .body("")
+        .send()
+        .await?;
+    }
+    }
+
+    Ok(())
+}
+
+/// Check every day at 00h00 if users need to be deleted
+pub async fn remove_deleted_account() {
+    tokio::task::spawn(async {
+        loop {
+            let now = Utc::now();
+            let time = (now.naive_utc().date().and_hms_opt(0, 0, 0).unwrap() + ChronoDuration::days(1)).timestamp()-now.timestamp();
+            std::thread::sleep(Duration::from_secs(time as u64));
+
+            if let Ok(x) = crate::database::cassandra::query(format!("SELECT user_id, platform FROM signaly.suspend WHERE expire_at = '{}'", now.format("%Y-%m-%d+0000")), vec![]) {
+                let res = x.get_body().unwrap().as_cols().unwrap().rows_content.clone();
+
+                for acc in res.iter() {
+                    match std::str::from_utf8(&acc[1].clone().into_plain().unwrap()[..]).unwrap() {
+                        "all" => {
+                            reqwest::Client::new().delete(format!("{}/users/{}", dotenv::var("AUTHA_URL").expect("Missing env `AUTHA_URL`"), std::str::from_utf8(&acc[0].clone().into_plain().unwrap()[..]).unwrap()))
+                            .header("Authorization", dotenv::var("GLOBAL_AUTH").expect("Missing env `GLOBAL_AUTH`"))
+                            .body("")
+                            .send()
+                            .await
+                            .unwrap();
+    
+                            for service in read().services {
+                                reqwest::Client::new().delete(format!("{}/account/deletion?vanity={}", service, std::str::from_utf8(&acc[0].clone().into_plain().unwrap()[..]).unwrap()))
+                                    .header("Authorization", dotenv::var("GLOBAL_AUTH").expect("Missing env `GLOBAL_AUTH`"))
+                                    .body("")
+                                    .send()
+                                    .await
+                                    .unwrap();
+                            }
+                        },
+                        _ => {
+                            reqwest::Client::new().delete(format!("{}account/deletion?vanity={}", std::str::from_utf8(&acc[1].clone().into_plain().unwrap()[..]).unwrap(), std::str::from_utf8(&acc[0].clone().into_plain().unwrap()[..]).unwrap()))
+                                .header("Authorization", dotenv::var("GLOBAL_AUTH").expect("Missing env `GLOBAL_AUTH`"))
+                                .body("")
+                                .send()
+                                .await
+                                .unwrap();
+                        }
+                    }
+                }
+            };
+        }
+    });
 }

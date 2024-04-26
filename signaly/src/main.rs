db512d1637bb3be5cf1a539f2a34abf1a7f8f4a7
Hinome <57831472+RealHinome@users.noreply.github.com>
2024-04-26 16:17:28 +0200

    feat: create Kafka producers and consumers (#7)
    Allow to create producers and consumers for Kafka pool.
    
    Signed-off-by: RealHinome <hinome@gravitalia.com>

--- signaly/src/main.rs
+++ signaly/src/main.rs
@@ -1,17 +1,45 @@
+//! Report and sanction aggregator to perform targeted research.
+mod helpers;
+mod router;
+
 use signaly_db::cassandra::Manager as ScyllaManager;
+use signaly_db::kafka::Manager as KafkaManager;
+use signaly_db::kafka::Type;
+use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
 async fn main() -> signaly_error::Result<()> {
-    println!("Hello, world!");
+    tracing_subscriber::registry()
+        .with(
+            fmt::layer()
+                .with_file(true)
+                .with_line_number(true)
+                .with_thread_ids(true),
+        )
+        .init();
 
-    let session = ScyllaManager::new(
-        vec!["127.0.0.1:9042".to_string()],
+    let scylla = ScyllaManager::new(
+        vec!["cassandra:9042".to_string()],
         Some("cassandra".to_string()),
         Some("cassandra".to_string()),
         10,
-    ).await;
+    )
+    .await;
 
-    println!("{:?}", session);
+    let kafka_hosts: Vec<String> = std::env::var("KAFKA_BROKERS")
+        .unwrap_or_else(|_| "localhost:9092".to_string())
+        .split(",")
+        .map(ToString::to_string)
+        .collect();
+
+    let kafka_producer =
+        KafkaManager::new(Type::Producer, None, kafka_hosts.clone()).await?;
+    let kafka_consumer = KafkaManager::new(
+        Type::Consumer,
+        Some(std::env::var("TOPIC").unwrap_or_else(|_| "*".to_string())),
+        kafka_hosts,
+    )
+    .await?;
 
     Ok(())
 }

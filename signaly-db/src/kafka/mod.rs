d8b16731f31a7c318e91a64e1c657fefd7b90437
Hinome <57831472+RealHinome@users.noreply.github.com>
2024-05-02 23:36:58 +0200

    chore: add send function to brokers (#8)
    * add send function
    
    * remove futures executor

--- signaly-db/src/kafka/mod.rs
+++ signaly-db/src/kafka/mod.rs
@@ -2,8 +2,17 @@
 
 mod pool;
 
+pub use kafka::consumer::Consumer;
+use kafka::{
+    consumer::{FetchOffset, GroupOffsetStorage},
+    producer::Record,
+    Error as KafkaError,
+};
 use pool::KafkaConnectionManager;
-use signaly_error::{DatabaseError::PoolCreation, Error, ErrorType};
+use signaly_error::{
+    DatabaseError::{PoolCreation, PoolObtention},
+    Error, ErrorType,
+};
 
 type Pool = deadpool::managed::Pool<KafkaConnectionManager>;
 
@@ -11,47 +20,80 @@ type Pool = deadpool::managed::Pool<KafkaConnectionManager>;
 #[allow(dead_code, missing_debug_implementations)]
 pub struct Manager {
     /// Pool session.
-    session: Pool,
-}
-
-/// Type of session to create.
-#[derive(Debug)]
-pub enum Type {
-    /// Creates a session to produce messages.
-    Producer,
-    /// Creates a session to consume messages.
-    Consumer,
+    pub session: Pool,
 }
 
 impl Manager {
-    /// Create a new pool of connections.
+    /// Create a new pool of producer connections.
     ///
     /// # Examples
     /// ```rust
     /// use signaly_db::kafka::Manager as KafkaManger;
     ///
     /// let session = KafkaManger::new(
-    ///     vec!["localhost:9092".to_string()]
+    ///     vec!["localhost:9092".to_string()],
+    ///     10,
     /// );
     /// // Do what ever you want with your cool new session...
     /// ```
     pub async fn new(
-        mode: Type,
-        topic: Option<String>,
         urls: Vec<String>,
+        pool_size: usize,
     ) -> Result<Self, Error> {
         Ok(Manager {
-            session: Pool::builder(KafkaConnectionManager::new(
-                urls, topic, mode,
-            ))
-            .build()
-            .map_err(|error| {
-                Error::new(
-                    ErrorType::Database(PoolCreation),
-                    Some(Box::new(error)),
-                    None,
-                )
-            })?,
+            session: Pool::builder(KafkaConnectionManager::new(urls))
+                .max_size(pool_size)
+                .build()
+                .map_err(|error| {
+                    Error::new(
+                        ErrorType::Database(PoolCreation),
+                        Some(Box::new(error)),
+                        None,
+                    )
+                })?,
         })
     }
+
+    /// Send a message via Kafka broker.
+    pub async fn send(
+        &self,
+        topic: String,
+        content: String,
+    ) -> Result<(), Error> {
+        self.session
+            .get()
+            .await
+            .map_err(|error| {
+                Error::new(
+                    ErrorType::Database(PoolObtention),
+                    Some(Box::new(error)),
+                    Some(
+                        "while trying to send a message via Kafka".to_string(),
+                    ),
+                )
+            })?
+            .send(&Record::from_value(topic.as_str(), content))
+            .map_err(|error| {
+                Error::new(
+                    ErrorType::Unspecified,
+                    Some(Box::new(error)),
+                    Some(
+                        "while trying to send a message via Kafka".to_string(),
+                    ),
+                )
+            })
+    }
+}
+
+/// Create a consumer connection.
+pub async fn new_consumer(
+    topic: String,
+    urls: Vec<String>,
+) -> Result<Consumer, KafkaError> {
+    Consumer::from_hosts(urls)
+        .with_topic(topic)
+        .with_group("my-group".to_string())
+        .with_fallback_offset(FetchOffset::Earliest)
+        .with_offset_storage(Some(GroupOffsetStorage::Kafka))
+        .create()
 }

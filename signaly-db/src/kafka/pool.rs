45aab4bcf3a23e266751801e93896c6fa84a8d76
Hinome <57831472+RealHinome@users.noreply.github.com>
2024-05-02 23:38:27 +0200

    fix: remove consumer from pool

--- signaly-db/src/kafka/pool.rs
+++ signaly-db/src/kafka/pool.rs
@@ -1,21 +1,13 @@
 use deadpool::managed;
 use kafka::{
-    consumer::{Consumer, FetchOffset, GroupOffsetStorage},
     producer::{Compression, Producer, Record, RequiredAcks},
     Error,
 };
 use std::{fmt::Write, time::Duration};
 
-pub enum Type {
-    Producer(Producer),
-    Consumer(Consumer),
-}
-
 #[derive(Debug)]
 pub struct KafkaConnectionManager {
     urls: Vec<String>,
-    mode: super::Type,
-    topic: Option<String>,
 }
 
 impl KafkaConnectionManager {
@@ -23,37 +15,21 @@ impl KafkaConnectionManager {
     ///
     /// See [`kafka::producer::Producer`] and [`kafka::consumer::Consumer`]
     /// for a description of the parameter types.
-    pub fn new(
-        urls: Vec<String>,
-        topic: Option<String>,
-        mode: super::Type,
-    ) -> KafkaConnectionManager {
-        KafkaConnectionManager { urls, mode, topic }
+    pub fn new(urls: Vec<String>) -> KafkaConnectionManager {
+        KafkaConnectionManager { urls }
     }
 }
 
 impl managed::Manager for KafkaConnectionManager {
-    type Type = Type;
+    type Type = Producer;
     type Error = Error;
 
     async fn create(&self) -> Result<Self::Type, Self::Error> {
-        match self.mode {
-            super::Type::Producer => Ok(Type::Producer(
-                Producer::from_hosts(self.urls.clone())
-                    .with_ack_timeout(Duration::from_secs(1))
-                    .with_required_acks(RequiredAcks::One)
-                    .with_compression(Compression::GZIP)
-                    .create()?,
-            )),
-            super::Type::Consumer => Ok(Type::Consumer(
-                Consumer::from_hosts(self.urls.clone())
-                    .with_topic(self.topic.clone().unwrap_or("*".to_string()))
-                    .with_group(String::default())
-                    .with_fallback_offset(FetchOffset::Earliest)
-                    .with_offset_storage(Some(GroupOffsetStorage::Kafka))
-                    .create()?,
-            )),
-        }
+        Producer::from_hosts(self.urls.clone())
+            .with_ack_timeout(Duration::from_secs(1))
+            .with_required_acks(RequiredAcks::One)
+            .with_compression(Compression::GZIP)
+            .create()
     }
 
     async fn recycle(
@@ -61,20 +37,15 @@ impl managed::Manager for KafkaConnectionManager {
         conn: &mut Self::Type,
         _: &managed::Metrics,
     ) -> managed::RecycleResult<Error> {
-        match conn {
-            Type::Producer(conn) => {
-                let mut buf = String::with_capacity(1);
-                let _ = write!(&mut buf, "{}", 0);
+        let mut buf = String::with_capacity(1);
+        let _ = write!(&mut buf, "{}", 0);
 
-                match conn.send(&Record::from_value("test", buf)) {
-                    Ok(_) => Ok(()),
-                    Err(error) => {
-                        tracing::error!(target: "signaly-db.kafka", "Connection could not be recycled: failed to send message to \"test\" topic.");
-                        Err(managed::RecycleError::message(error.to_string()))
-                    },
-                }
+        match conn.send(&Record::from_value("test", buf)) {
+            Ok(_) => Ok(()),
+            Err(error) => {
+                tracing::error!(target: "signaly-db.kafka", "Connection could not be recycled: failed to send message to \"test\" topic.");
+                Err(managed::RecycleError::message(error.to_string()))
             },
-            Type::Consumer(_) => Ok(()),
         }
     }
 }
